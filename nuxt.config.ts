90180fa09eec66d8b61ceafcff4de7cdd3a816d5
alan tecuapetla <alan@digitomenu.com>
2024-05-04 19:56:40 +0900

    images folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,48 @@ export default defineNuxtConfig({
       '@nuxtjs/i18n',
       '@nuxtjs/cloudinary',
     ],
-  // cloudinary: {
-  //   cloudName: 'nuxt-cloudinary'
-  // },
+  cloudinary: {
+    cloudName: 'nuxt-cloudinary'
+  },
   pages: true,
   srcDir: 'src/',
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta:[
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://res.cloudinary.com/dd05o0vvo/image/upload/v1713794636/AlanTecuapetla-OGimage_gyzcpy.png'
+        }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: 'https://cdn.jsdelivr.net/npm/modern-normalize@2.0.0/modern-normalize.min.css',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/images/favicon-32x32.png'
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdn.plyr.io/3.6.8/plyr.css"
+        }
+      ]
+    },
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in' // default
+    },
+    layoutTransition: {
+      name: 'layout',
+      mode: 'out-in' // default
+    },
+  },
   i18n: {
     vueI18n: './i18n.config.ts'
   }
