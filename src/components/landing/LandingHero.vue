b6451da27dcb43395e0aee649d03b67c837d7c64
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-06-24 23:16:09 +0900

    LandingHero.vue

---
+++ src/components/landing/LandingHero.vue
@@ -0,0 +1,15 @@
+<template>
+  <header className="navbar">
+    <nav className="nav container">
+    {/* <NavLink to="/" className="nav__logo"></NavLink> */}
+      <div className="nav__menu">
+      </div>
+      <div className="nav icons">
+        <NavLink to="/contact" className="nav__contact-icon"></NavLink>
+        <a className="icon__github" rel="stylesheet" href="https://github.com/Alan-Tecua" target="blank_"/>
+        <a className="icon__linkedin" rel="stylesheet" href="https://www.linkedin.com/in/alan-tecuapetla/" target="blank_"/>
+      </div>
+    </nav>
+  </header>
+
+</template>
